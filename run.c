e76f2216c09d12475eb8eba9ce7146ec7c1800a8
James Smith <jismith1989@googlemail.com>
2017-01-29 20:49:23 +0000

    Retabulate "run.c" file.

--- run.c
+++ run.c
@@ -13,55 +13,55 @@
 
 void run(const char *bfString)
 {
-	int8_t cells[CELL_COUNT] = { '\0' },
-		*ptr = &cells[0];
+    int8_t cells[CELL_COUNT] = { '\0' },
+        *ptr = &cells[0];
 
-	int32_t i, j, nestedJumps;
-	char c;
-	pid_t pid;
-	for ( i = 0; (c = *(bfString + i)) != '\0'; ++i )
-	{
-		switch (c)
-		{ /* cells and values wrap-around within respective bounds */
-        	case 0x2B /* '+' */:    ( *ptr < 0xFF ) ? ++*ptr : (*ptr = 0); break;
-        	case 0x2D /* '-' */:    ( *ptr > 0 ) ? --*ptr : (*ptr = 0xFF); break;
-        	case 0x3E /* '>' */:    ( ptr < cells + CELL_COUNT - 1 ) ? ++ptr : (ptr = cells); break;
-        	case 0x3C /* '<' */:    ( ptr > cells ) ? --ptr : (ptr = cells + CELL_COUNT - 1); break;
-        	case 0x2E /* '.' */:    putchar(*ptr); break;
-        	case 0x2C /* ',' */:    *ptr = getchar(); break;
-        	case 0x5B /* '[' */:
-				if ( !*ptr ) /* zero value */
-				{
-					for ( nestedJumps = 0; (c = *(bfString + i)) != '\0'; ++i)
-					{
-						if ( c == 0x5B )
-							++nestedJumps;
-						else if ( c == 0x5D && --nestedJumps == 0 )
-							break;
-					}
-				}
-				break;
-        	case 0x5D /* ']' */:
-				if ( *ptr ) /* non-zero value */
-				{
-					for ( nestedJumps = 0; i >= 0; --i )
-					{
-						if ( (c = *(bfString + i)) == 0x5D )
-							++nestedJumps;
-						else if ( c == 0x5B && --nestedJumps == 0 )
-							break;
-					}
-				}
-            	break;
-			case 0x59 /* 'Y' */:
-				if ( (pid = fork()) < 0 )
-					errExit(errno, "Fork failed");
-				( pid ) ? (*ptr = 0) /* parent */ : (*(++ptr) = 1);
-    	        break;
-			default: /* ignore other characters */
-				break;
-    	}
-	}
+    int32_t i, j, nestedJumps;
+    char c;
+    pid_t pid;
+    for ( i = 0; (c = *(bfString + i)) != '\0'; ++i )
+    {
+        switch (c)
+        { /* cells and values wrap-around within respective bounds */
+            case 0x2B /* '+' */:    ( *ptr < 0xFF ) ? ++*ptr : (*ptr = 0); break;
+            case 0x2D /* '-' */:    ( *ptr > 0 ) ? --*ptr : (*ptr = 0xFF); break;
+            case 0x3E /* '>' */:    ( ptr < cells + CELL_COUNT - 1 ) ? ++ptr : (ptr = cells); break;
+            case 0x3C /* '<' */:    ( ptr > cells ) ? --ptr : (ptr = cells + CELL_COUNT - 1); break;
+            case 0x2E /* '.' */:    putchar(*ptr); break;
+            case 0x2C /* ',' */:    *ptr = getchar(); break;
+            case 0x5B /* '[' */:
+                if ( !*ptr ) /* zero value */
+                {
+                    for ( nestedJumps = 0; (c = *(bfString + i)) != '\0'; ++i)
+                    {
+                        if ( c == 0x5B )
+                            ++nestedJumps;
+                        else if ( c == 0x5D && --nestedJumps == 0 )
+                            break;
+                    }
+                }
+                break;
+            case 0x5D /* ']' */:
+                if ( *ptr ) /* non-zero value */
+                {
+                    for ( nestedJumps = 0; i >= 0; --i )
+                    {
+                        if ( (c = *(bfString + i)) == 0x5D )
+                            ++nestedJumps;
+                        else if ( c == 0x5B && --nestedJumps == 0 )
+                            break;
+                    }
+                }
+                break;
+            case 0x59 /* 'Y' */:
+                if ( (pid = fork()) < 0 )
+                    errExit(errno, "Fork failed");
+                ( pid ) ? (*ptr = 0) /* parent */ : (*(++ptr) = 1);
+                break;
+            default: /* ignore other characters */
+                break;
+        }
+    }
 
-	printf("\n");
+    printf("\n");
 }
